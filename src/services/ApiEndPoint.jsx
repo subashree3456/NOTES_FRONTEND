fe27bed05ddbf9d623f261055740baaff7d2a40c
subashree3456 <subashree3003@gmail.com>
2024-11-27 23:35:29 +0530

    comit msg

--- src/services/ApiEndPoint.jsx
+++ src/services/ApiEndPoint.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-    baseURL: 'http://localhost:7000',
+    baseURL: 'https://notes-backend-1-vmmt.onrender.com',
     headers: {
         'Content-Type': 'application/json' // Removed extra space
     },
@@ -14,5 +14,9 @@ export const get = (url, params) => instance.get(url, { params });
 export const post = (url, data) => instance.post(url, data);
 export const put = (url, data) => instance.put(url, data);
 export const dele = (url) => instance.delete(url);
+
+
+
+// baseURL: 'http://localhost:7000',
 
 
