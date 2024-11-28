fdaa2d8d3e1a3c6a7e24f353f151fbe0898c8077
subashree3456 <subashree3003@gmail.com>
2024-11-28 09:50:36 +0530

    comit msgk

--- src/services/ApiEndPoint.jsx
+++ src/services/ApiEndPoint.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-    baseURL: 'https://notes-backend-1-vmmt.onrender.com',
+    baseURL: 'https://notes-backend-9mzm.onrender.com',
     headers: {
         'Content-Type': 'application/json' // Removed extra space
     },
